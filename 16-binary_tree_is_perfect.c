e4f2e8756fe59ca7013e2ab99b8ba851b080385b
fredhii <fredhiixd@gmail.com>
2020-07-21 23:18:58 -0500

    fix perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,56 +1,53 @@
 #include "binary_trees.h"
 
 /**
- * _bite_height - Mearure the height of a binary tree
- * @tree: Binary tree
- * Return: Binary tree height
+ * bte_is_leaf - Check if node is a leaf in binary tree
+ * @node: Node to check
+ * Return: 1 if is leaf, 0 otherwise
  */
-size_t _bite_height(const binary_tree_t *tree)
+unsigned char bte_is_leaf(const binary_tree_t *node)
 {
-	size_t tLeft = 0, tRight = 0;
-
-	if (tree)
-	{
-		tLeft = tree->left ? 1 + _bite_height(tree->left) : 1;
-		tRight = tree->right ? 1 + _bite_height(tree->right) : 1;
-		return ((tLeft > tRight) ? tLeft : tRight);
-	}
-	return (0);
+	return ((node->left == NULL && node->right == NULL) ? 1 : 0);
 }
 
 /**
- * _bite_balance - Measures balance factor of a binary tree
+ * depth - Returns the depth of a BT
  * @tree: Binary tree
- * Return: Tree balance factor
+ * Return: Depth from node
  */
-int _bite_balance(const binary_tree_t *tree)
+size_t depth(const binary_tree_t *tree)
 {
-	if (tree)
-		return (_bite_height(tree->left) - _bite_height(tree->right));
-
-	return (0);
+	return (tree->parent != NULL ? 1 + depth(tree->parent) : 0);
 }
 
 /**
- * _bt_is_full - Checks if a binary tree is full
+ * get_leaf - Returns a leaf of a binary tree.
  * @tree: Binary tree
- * Return: 1 if is full, 0 otherwise
+ * Return: Pointer to leaf
  */
-int _bt_is_full(const binary_tree_t *tree)
+const binary_tree_t *get_leaf(const binary_tree_t *tree)
 {
-	int lSize, rSize;
+	if (bte_is_leaf(tree) == 1)
+		return (tree);
+	return (tree->left ? get_leaf(tree->left) : get_leaf(tree->right));
+}
 
-	if (tree)
-	{
-		if (!tree->right && !tree->left)
-			return (1);
-
-		lSize = _bt_is_full(tree->left);
-		rSize = _bt_is_full(tree->right);
-
-		return ((lSize == 0 || rSize == 0) ? 0 : 1);
-	}
-	return (0);
+/**
+ * _check_if_perfect - Checks if a binary tree is perfect
+ * @tree: Binary tree
+ * @leaf_depth: Depth node
+ * @level: Current node
+ * Return: If the tree is perfect, 1, otherwise 0.
+ */
+int _check_if_perfect(const binary_tree_t *tree,
+		size_t leaf_depth, size_t level)
+{
+	if (bte_is_leaf(tree))
+		return (level == leaf_depth ? 1 : 0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	return (_check_if_perfect(tree->left, leaf_depth, level + 1) &&
+		_check_if_perfect(tree->right, leaf_depth, level + 1));
 }
 
 /**
@@ -60,8 +57,7 @@ int _bt_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree)
-		if (_bt_is_full(tree) && _bite_balance(tree) == 0)
-			return (1);
-	return (0);
+	if (tree == NULL)
+		return (0);
+	return (_check_if_perfect(tree, depth(get_leaf(tree)), 0));
 }
